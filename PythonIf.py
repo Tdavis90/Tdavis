219e3ffafc41eb3ec6289d1fa923acc2a89f2bbc
Tdavis90 <60702450+Tdavis90@users.noreply.github.com>
2020-02-12 08:20:47 -0500

    Add files via upload

---
+++ PythonIf.py
@@ -0,0 +1,11 @@
+Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 14 2019, 23:09:19) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=1
+>>> b=2
+>>> if a > b:
+	print(a)
+else:
+	print(b)
+commit program to CS10401 repository.
+SyntaxError: invalid syntax
+>>> verify to online repository
